cd1bcf4b843475e31e9770d6292face36e852b66
Aldiiar <aldiarm27@gmail.com>
2023-02-14 23:26:39 +0600

    done hw1

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import HttpResponse
+import datetime
+
+def hello_view(request):
+    if request.method == 'GET':
+        return HttpResponse("Hello! Its my project")
+
+def nowdate_view(request):
+    if request.method == 'GET':
+        return HttpResponse(datetime.date.today())
+
+def goodby_view(request):
+    if request.method == 'GET':
+        return HttpResponse("Goodby user!")
+
+
